d923ffc01f60b82fbc30cd6a2d20e0288839db8e
Matthew Malone <47948268+ohnomalone@users.noreply.github.com>
2024-09-04 10:09:15 -0700

    chore: clean up header removing nav (#2)
    Clean up header by removing navigation
    install MUI data grid for full MUI component support

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,11 +1,11 @@
 import { Box, Stack } from "@mui/material";
 import { Outlet } from "react-router-dom";
-import HeaderNav from "../HeaderNav/HEaderNav";
+import Header from "../Header/Header";
 
 const MainLayout = () => {
   return (
     <Stack direction="column" sx={{ height: "100vh", overflow: "hidden" }}>
-      <HeaderNav />
+      <Header />
       <Box component="main" sx={{ flexGrow: 1, overflow: "auto" }}>
         <Outlet />
       </Box>
--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,9 +1,11 @@
-import { Box, Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 export const Home = () => {
   return (
-    <Box>
-      <Typography variant="h1">Home</Typography>
-    </Box>
+    <Container maxWidth="lg">
+      <Typography variant="h3" component="h1">
+        Home
+      </Typography>
+    </Container>
   );
 };
