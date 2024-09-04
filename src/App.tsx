bf0cf11e26028b170d5ab550221dac0b65397da1
Matthew Malone <m.malone@me.com>
2024-09-03 18:43:45 -0700

    feat: add tanstack react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ThemeConfigProvider as ThemeProvider } from "./theme/ThemeProvider";
+import { queryClient } from "./query/queryClient";
 
 function App() {
   return (
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
